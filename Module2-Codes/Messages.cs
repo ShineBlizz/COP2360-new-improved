eeb74442f4469a4da704c7a71cc497dc05287665
[Nate Hill] <Nathan.earl.hill@gmail.com>
2025-02-16 10:34:42 -0500

    Module 3 bunny code

---
+++ Module2-Codes/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_Codes
+{
+    static class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to the program");
+        }
+        public static void Waiting()
+        {
+            Console.WriteLine("Hello! i am waiting");
+        }
+        public static void bye()
+        {
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
