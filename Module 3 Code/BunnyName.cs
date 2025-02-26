0a093bef19fd53e3b58d80877e7425e30d0cefdc
ShineBlizz <162535778+ShineBlizz@users.noreply.github.com>
2025-02-26 11:42:44 -0500

    Update BunnyName.cs

--- Module 3 Code/BunnyName.cs
+++ Module 3 Code/BunnyName.cs
@@ -1,18 +1,37 @@
 ﻿using System;
 
-class Person
+// Class definition
+public class Bunny
 {
-    private string name;
-    private int age;
+    public string Name { get; private set; } // "get: private set" means that the values can be read from outside the class
+    public bool LikesCarrots { get; private set; } // It also means that it can only be modified inside the class and not from the outside
+    public bool LikesHumans { get; private set; }
 
-    public Person(string name, int age)
+
+    private static Random random = new Random(); // Static to prevent reseeding issues
+    private static string[] names = { "Timmy", "Jimmy", "Thimmy", "Shimmy", "Simmy", "Mashimmy", "Limmy", "Dimmy", "Rimmy", "Kimmy", "Zimmy", "Blimmy", "Chimmy", "Grimmy", "Whimmy" };
+
+    public Bunny()
     {
-        this.name = "Alex";
-        this.age = 21;
+        Name = names[random.Next(names.Length)]; // Picks a random name from the array
+        LikesCarrots = random.Next(2) == 1; // 50/50 chance
+        LikesHumans = random.Next(2) == 1; // 50/50 chance again
     }
-
-    public void Introduce()
+    // This is our method to actually print out the Bunnys information
+    public void PrintBunnyInfo()
     {
-        Console.WriteLine($"Hi, my name is {name} and I am {age} years old.");
+        Console.WriteLine($"Bunny Name: {Name}");
+        Console.WriteLine($"Likes Carrots?: {LikesCarrots}");
+        Console.WriteLine($"Likes Humans?: {LikesHumans}");
+    }
+}
+
+// This is our main program
+class Program()
+{
+    static void Main()
+    {
+        Bunny myBunny = new Bunny(); // This will create a new bunny with random attributes
+        myBunny.PrintBunnyInfo(); // This outputs its details
     }
 }
